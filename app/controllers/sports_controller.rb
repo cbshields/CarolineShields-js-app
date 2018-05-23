d0929339f7b78f72b0924c5a955e702a6fdb3e88
cbshields <cbshields@outlook.com>
2018-05-22 20:39:22 -0400

    finished basic views/controllers for sports

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -4,7 +4,40 @@ class SportsController < ApplicationController
     @sports = Sport.all
   end
 
+  def new
+    @sport = Sport.new
+  end
+
+  def create
+    @sport = Sport.create(sport_params)
+    if @sport.save
+      redirect_to sport_path(@sport)
+    else
+      render :new
+    end
+  end
+
   def show
     @sport = Sport.find(params[:id])
+  end
+
+  def edit
+    @sport = Sport.find(params[:id])
+  end
+
+  def update
+    @sport = Sport.find(params[:id])
+    @sport.update(sport_params)
+    if @sport.save
+      redirect_to sport_path(@sport)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+  def sport_params
+      params.require(:sport).permit(:name)
   end
 end
