6e50d6842451f2554486d9ea68c7b3d0ef4de6fe
cbshields <cbshields@outlook.com>
2018-05-26 13:17:47 -0400

    removed test code

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -10,13 +10,7 @@ before_action :find_user, only: [:show, :edit, :update, :destroy]
   end
 
   def create
-    # binding.pry
-    # player_params = player_params[:player]
-    # sports_params = player_params[:sports]
-
-    # @sport = Sport.create(sports_params)
     @player = Player.create(player_params)
-    # Position.create(sport_id: @sport.id, player_id: @player.id)
     if @player.save
       redirect_to player_path(@player)
     else
