003b5ee232b783e7997fe1361d8a01c7b81aa2e0
cbshields <cbshields@outlook.com>
2018-06-23 14:53:28 -0700

    trying to get positions/sports to view for each player

--- app/models/player.rb
+++ app/models/player.rb
@@ -7,27 +7,28 @@ scope :over_18, lambda {where('age >= ?',18)}
   has_many :positions
   has_many :sports, through: :positions
 
-  # accepts_nested_attributes_for :sports, reject_if: :all_blank
-  # accepts_nested_attributes_for :positions, reject_if: :all_blank
+  accepts_nested_attributes_for :sports, reject_if: :all_blank
+  accepts_nested_attributes_for :positions, reject_if: :all_blank
 
   def sports_attributes=(sport_attributes)
-    binding.pry
    sport_attributes.values.each do |sport_attribute|
      #better way to do this?
+      if !sport_attribute[name].nil?
        sport = Sport.find_or_create_by(sport_attribute)
        self.sports << sport
      end
-
+   end
  end
 
 
   def positions_attributes=(position_attributes)
-    binding.pry
    position_attributes.values.each do |position_attribute|
+       if !position_attribute[name].nil?
        position = Position.find_or_create_by(position_attribute)
        self.positions << position
      end
    end
+ end
 
 
 
