95520fb4f9146579948a1763c033e3da593d587c
cbshields <cbshields@outlook.com>
2019-04-16 11:51:58 -0700

    removed sport class and created one Player class with address, age, and sports attributes

--- app/assets/javascripts/players.js
+++ app/assets/javascripts/players.js
@@ -8,7 +8,7 @@ $(function(){
       url: this.href,
       dataType: "json"
     }).done(function(response) {
-      const playerSport = new Sport(response)
+      const playerSport = new Player(response)
       // player.playerhHTML()
       //debugger
       //do I need to create 2 constructor objects or have everything I need in one?
@@ -24,28 +24,12 @@ $(function(){
       url: this.href,
       dataType: "json"
     }).done(function(response){
-      debugger
+      const player = new Player(response)
+      $(`div.player_info-${player.id}`).html(player.playerHTML())
     })
   })
 })
 
-
-class Sport {
-  constructor(obj) {
-    this.id = obj.id
-    this.name = obj.name
-    this.sports = obj.sports
-  }
-}
-
-Sport.prototype.sportHTML = function() {
-  let sports = this.sports
-    // console.log(`here is the sports array: ${sports}`)
-  return sports.map(function(sport) {
-    //console.log(`here is the singular instance of sport: ${sport.name}`)
-    return (`<li>${sport.name}</li>`)
-  })
-}
 
 class Player {
   constructor(obj) {
@@ -53,5 +37,23 @@ class Player {
     this.name = obj.name
     this.age = obj.age
     this.address = obj.address
+    this.sports = obj.sports
   }
+}
+Player.prototype.sportHTML = function() {
+  let sports = this.sports
+    // console.log(`here is the sports array: ${sports}`)
+  return sports.map(function(sport) {
+    //console.log(`here is the singular instance of sport: ${sport.name}`)
+    return (`<p>${sport.name}</p>`)
+  })
+}
+
+
+Player.prototype.playerHTML = function() {
+  return (`
+    <p>Age: ${this.age}</p>
+    <p>Address: ${this.address}</p>
+
+    `)
 }
